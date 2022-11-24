ad608a06795dff97b466dbbfc95a427cb32146a9
010203le <44265695+010203le@users.noreply.github.com>
2022-11-24 20:27:20 +0800

    Update getFrames.py

--- getFrames.py
+++ getFrames.py
@@ -5,41 +5,41 @@ def timeToFrame(time):
     seconds = int(time[2].split(".")[0])
     milliseconds = int(time[2].split(".")[1])
     frame = (hours * 60 * 60 * 23.976) + (minutes * 60 * 23.976) + (seconds * 23.976) + (milliseconds * 23.976 / 1000)
-    #無條件捨去小數點後的數字(從0開始計算)。
+    #python count from 0
     return int(frame)
 
 #load chapter file
-with open("chapters.txt", "r") as f:
+chapterFile = input('Input chapter file path(.txt) : ') 
+with open(chapterFile, "r") as f:
     chapters = f.readlines()
     chapters = chapters[0::2]
     chapters = [x.strip() for x in chapters]
     chapters = [x.split("=") for x in chapters]
     chapters = {x[0]:x[1] for x in chapters}
 
-#計算影格數
-for chapter in chapters:
-    chapters[chapter] = timeToFrame(chapters[chapter])
+#count frames
+try:
+    for chapter in chapters:
+        chapters[chapter] = timeToFrame(chapters[chapter])
+except:
+    raise ValueError('ReplaceNC: chapter file\'s format is not correct!')
 
 cf = list(chapters.values())
 opt = []
-#保留間格符合條件項目
+#keep op or ed frame range
 for i in range(len(cf)):
     try:
         if cf[i+1] - cf[i] < 2160:
             if cf[i+1] - cf[i] > 2154:
-                opt.append(str('"['+str(cf[i])+' '+str(cf[i+1])+']"'))
+                opt.append(str('['+str(cf[i])+' '+str(cf[i+1])+']'))
     except:
-        opt.append(str('"['+str(cf[i])+' '+str(cf[i]+2157)+']"'))
+        opt.append(str('['+str(cf[i])+' '+str(cf[i]+2157)+']'))
 
 
-try:
-    print(opt[0])
-except:
-    print('本集沒有OP/ED')
+print(opt[0])
+
 
 try:
     print(opt[1])
 except:
-    print('本集沒有OP or ED')
-
-
+    print('This clip must not have op or ed!')
