ca30784e25e320f75165eebe4bd97ad16234467b
010203le <44265695+010203le@users.noreply.github.com>
2022-11-30 19:42:25 +0800

    Update getFrames.py

--- getFrames.py
+++ getFrames.py
@@ -1,3 +1,44 @@
+def getFrameRange(chapterFile, mode):
+    try:
+        with open(chapterFile, 'r') as f:
+            chapters = f.readlines()
+            chapters = chapters[0::2]
+            chapters = [x.strip() for x in chapters]
+            chapters = [x.split("=") for x in chapters]
+            chapters = {x[0]:x[1] for x in chapters}
+    except:
+        raise ValueError('ReplaceNC: chapter file is not correct!')
+
+    try:
+        for chapter in chapters:
+            chapters[chapter] = timeToFrame(chapters[chapter])
+    except:
+        raise ValueError('ReplaceNC: chapter file\'s format is not correct!')
+
+    cf = list(chapters.values())
+    opt = []
+
+    for i in range(len(cf)):
+        try:
+            if cf[i+1] - cf[i] < 2160:
+                if cf[i+1] - cf[i] > 2154:
+                    opt.append('['+str(cf[i])+' '+str(cf[i+1])+']')
+        except:
+            opt.append('['+str(cf[i])+' '+str(cf[i]+2157)+']')
+    
+    if mode == 'op':
+        return opt[0]
+    elif mode == 'ed':
+        try:
+            return opt[1]
+        except:
+            raise TypeError('ReplaceNC: This clip must not have op or ed!')
+    elif mode == 'all':
+        return opt[0] + ' ' + opt[1]
+    else:
+        raise ValueError('ReplaceNC: mode is wrong!')
+
+
 def timeToFrame(time):
     time = time.split(":")
     hours = int(time[0])
@@ -5,41 +46,11 @@ def timeToFrame(time):
     seconds = int(time[2].split(".")[0])
     milliseconds = int(time[2].split(".")[1])
     frame = (hours * 60 * 60 * 23.976) + (minutes * 60 * 23.976) + (seconds * 23.976) + (milliseconds * 23.976 / 1000)
-    #python count from 0
+    #frames count from 0
     return int(frame)
+
 
 #load chapter file
 chapterFile = input('Input chapter file path(.txt) : ') 
-with open(chapterFile, "r") as f:
-    chapters = f.readlines()
-    chapters = chapters[0::2]
-    chapters = [x.strip() for x in chapters]
-    chapters = [x.split("=") for x in chapters]
-    chapters = {x[0]:x[1] for x in chapters}
 
-#count frames
-try:
-    for chapter in chapters:
-        chapters[chapter] = timeToFrame(chapters[chapter])
-except:
-    raise ValueError('ReplaceNC: chapter file\'s format is not correct!')
-
-cf = list(chapters.values())
-opt = []
-#keep op or ed frame range
-for i in range(len(cf)):
-    try:
-        if cf[i+1] - cf[i] < 2160:
-            if cf[i+1] - cf[i] > 2154:
-                opt.append(str('['+str(cf[i])+' '+str(cf[i+1])+']'))
-    except:
-        opt.append(str('['+str(cf[i])+' '+str(cf[i]+2157)+']'))
-
-
-print(opt[0])
-
-
-try:
-    print(opt[1])
-except:
-    print('This clip must not have op or ed!')
+print(getFrameRange(chapterFile, 'all'))
